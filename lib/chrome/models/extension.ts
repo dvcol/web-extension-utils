fd1251eb664038ab7e6a2233bdf058b1799bb5bd
Dinh-Van Colomban <van.colomban@gmail.com>
2023-03-05 16:13:09 +0100

    feat(scripts): add script injection API

--- lib/chrome/models/extension.ts
+++ lib/chrome/models/extension.ts
@@ -1,1 +1,17 @@
 export type InstalledDetails = chrome.runtime.InstalledDetails;
+
+export type Manifest = chrome.runtime.Manifest;
+export type ManifestV2 = chrome.runtime.ManifestV2;
+export type ManifestV3 = chrome.runtime.ManifestV3;
+
+export type ContentScript = {
+  matches?: string[] | undefined;
+  exclude_matches?: string[] | undefined;
+  css?: string[] | undefined;
+  js?: string[] | undefined;
+  run_at?: string | undefined;
+  all_frames?: boolean | undefined;
+  match_about_blank?: boolean | undefined;
+  include_globs?: string[] | undefined;
+  exclude_globs?: string[] | undefined;
+};
--- lib/chrome/utils/extension.ts
+++ lib/chrome/utils/extension.ts
@@ -1,6 +1,6 @@
 import { fromEventPattern } from 'rxjs';
 
-import type { InstalledDetails } from '../models';
+import type { ContentScript, InstalledDetails } from '../models';
 
 import type { Observable } from 'rxjs';
 
@@ -13,3 +13,27 @@ const removeOnInstallHandler = (handler: InstalledHandler) => chrome.runtime.onI
  * @see chrome.runtime.onInstalled
  */
 export const onInstalled$: Observable<InstalledDetails> = fromEventPattern<InstalledDetails>(addOnInstallHandler, removeOnInstallHandler);
+
+/** @see chrome.runtime.getManifest */
+export const { getManifest } = chrome.runtime;
+
+const injectScript = (tabId: number, script: string[]) =>
+  chrome.scripting.executeScript({
+    target: { tabId },
+    files: script,
+  });
+
+const injectScripts = async (matches: ContentScript['matches'], scripts: ContentScript['js']) => {
+  const tabs = await chrome.tabs.query({ url: matches });
+  console.debug('Injecting content scripts in tabs', { tabs, matches, scripts });
+  tabs?.forEach(tab => {
+    if (!tab?.id) return;
+    if (!tab?.url?.startsWith('http')) return;
+    if (scripts) injectScript(tab.id, scripts);
+  });
+};
+
+export const injectContentScripts = () =>
+  getManifest()?.content_scripts?.forEach(async script => {
+    if (script?.js?.length) await injectScripts(script.matches, script.js);
+  });
