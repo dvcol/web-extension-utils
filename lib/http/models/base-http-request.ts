20a9e746e3309ea73517d0f219e66df0ed7c66d3
Dinh-Van Colomban <van.colomban@gmail.com>
2023-03-08 14:20:40 +0100

    fix(http): fix rxFetch destructuring

--- lib/http/models/base-http-request.ts
+++ lib/http/models/base-http-request.ts
@@ -1,11 +1,11 @@
 import type { HttpBody, HttpHeaders, HttpMethod, HttpParameters } from '@lib/http/models';
 
 /** Base Http request interface */
-export interface BaseHttpRequest {
+export interface BaseHttpRequest extends RequestInit {
   url: string | URL | { path: string | URL; base: string | URL };
+  params?: HttpParameters;
   method?: HttpMethod;
   headers?: HttpHeaders;
-  params?: HttpParameters;
   body?: HttpBody;
   redirect?: RequestRedirect;
 }
--- lib/http/utils/rxjs-fetch.ts
+++ lib/http/utils/rxjs-fetch.ts
@@ -7,7 +7,7 @@ import { fromFetch } from 'rxjs/fetch';
 import type { BaseHttpRequest } from '@lib/http/models';
 import type { Observable } from 'rxjs';
 
-export const rxFetch = <T>({ url, method, headers, params, body, redirect }: BaseHttpRequest): Observable<T> => {
+export const rxFetch = <T>({ url, params, redirect, ...init }: BaseHttpRequest): Observable<T> => {
   let _url: string;
   try {
     _url = buildUrl(url, params).toString();
@@ -16,9 +16,7 @@ export const rxFetch = <T>({ url, method, headers, params, body, redirect }: Bas
     return throwError(() => error);
   }
   return fromFetch<T>(_url, {
-    method,
-    headers,
-    body,
+    ...init,
     redirect: redirect ?? 'follow',
     selector: res => res.json(),
   });
