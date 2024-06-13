fbd0d8a7e1032ac881fd439b0d952067117aabe0
Dinh-Van Colomban <van.colomban@gmail.com>
2024-06-13 20:51:26 +0200

    fix(exports): rename node/file export path and tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['lib/index.ts', 'lib/chrome', 'lib/http', 'lib/common'],
+  entry: ['lib', '!lib/**/*.test.ts', '!lib/mocks'],
   format: ['cjs', 'esm'],
   sourcemap: false,
   clean: true,
