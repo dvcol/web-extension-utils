f45c03ffe1883d70929402b2598c0e7b2fff4871
Dinh-Van Colomban <van.colomban@gmail.com>
2024-08-08 21:30:36 +0200

    feat(chrome): adds to action utils

--- lib/chrome/utils/action.utils.ts
+++ lib/chrome/utils/action.utils.ts
@@ -1,1 +1,20 @@
+/** @see chrome.action */
 export const action: typeof chrome.action | undefined = globalThis?.chrome?.action;
+
+/** @see chrome.action.setIcon */
+export const setIcon: typeof chrome.action.setIcon | undefined = globalThis?.chrome?.action?.setIcon;
+
+/** @see chrome.action.setTitle */
+export const setTitle: typeof chrome.action.setTitle | undefined = globalThis?.chrome?.action?.setTitle;
+
+/** @see chrome.action.setBadgeText */
+export const setBadgeText: typeof chrome.action.setBadgeText | undefined = globalThis?.chrome?.action?.setBadgeText;
+
+/** @see chrome.action.setBadgeTextColor */
+export const setBadgeTextColor: typeof chrome.action.setBadgeTextColor | undefined = globalThis?.chrome?.action?.setBadgeTextColor;
+
+/** @see chrome.action.setBadgeBackgroundColor */
+export const setBadgeBackgroundColor: typeof chrome.action.setBadgeBackgroundColor | undefined = globalThis?.chrome?.action?.setBadgeBackgroundColor;
+
+/** @see chrome.action.openPopup */
+export const openPopup: typeof chrome.action.openPopup | undefined = globalThis?.chrome?.action?.openPopup;
