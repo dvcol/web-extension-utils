540252fd2b349bb51d34747913a26cc798b9f228
Dinh-Van Colomban <van.colomban@gmail.com>
2025-04-14 17:55:53 +0200

    fix(storage): adjust type signatures

--- lib/chrome/models/extension.model.ts
+++ lib/chrome/models/extension.model.ts
@@ -1,7 +1,7 @@
 export type EventTypes = `${chrome.runtime.OnInstalledReason}`;
 export type InstalledDetails = chrome.runtime.InstalledDetails;
 export type VersionUpdateDetails = InstalledDetails & { nextVersion: string };
-export type VersionUpdateCallback = (details: VersionUpdateDetails) => void;
+export type VersionUpdateCallback = (details: VersionUpdateDetails) => void | Promise<void>;
 
 export type Manifest = chrome.runtime.Manifest;
 export type ManifestV2 = chrome.runtime.ManifestV2;
